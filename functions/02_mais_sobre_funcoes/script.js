a2c224a7a48725fb3d814a34f5b1ef821731c1eb
Gilberto Mascena <gilbertomascena@gmail.com>
2023-04-27 22:12:21 -0300

    Add new filest

---
+++ functions/02_mais_sobre_funcoes/script.js
@@ -0,0 +1,22 @@
+function multiplicarTresNumeros(x, y, z) {
+    return x * y * z;
+}
+
+console.log(multiplicarTresNumeros(2, 3, 4));
+
+const mult = multiplicarTresNumeros(5, 4, 8);
+
+console.log("O valor de mult é: " + mult);
+
+function podeDirigir(idade, cnh) {
+    if (idade >= 18  && cnh == true) {
+        console.log("pode dirigir");
+    } else {
+        console.log("Não pode dirigir");
+    }
+}
+
+console.log(podeDirigir(19, true));
+console.log(podeDirigir(25, true));
+console.log(podeDirigir(44, 0)); // Aqui o zero reprezenta false.
+console.log(podeDirigir(19, 1)); // Aqui 1 zero reprezenta true.
